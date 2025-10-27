73cae89b143519bd36aba2aba5abe05eaeea271f
seahal <m.shiihara@gmail.com>
2025-10-27 19:54:42 +0900

    [checkpoint] daily update.

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,9 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn it_works() {
-        assert_eq!(crate::hw(), String::from("Hello, Client!"));
+        assert_eq!(hw(), String::from("Hello, Client!"));
     }
 }
